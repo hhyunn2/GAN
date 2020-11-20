f86e79275c95e9c30e494d342aba2b7ae82ea7d6
hhyunn2 <53824579+hhyunn2@users.noreply.github.com>
2020-11-20 16:44:36 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,46 @@
+def conv2d(params_list, instance_norm = True):
+    channel_in, channel_out, kernel_size, stride, padding, activation = params_list
+    layers = []
+    if instance_norm:
+        layers += [nn.Conv2d(channel_in, channel_out, kernel_size, stride, padding, bias=False),
+                   nn.InstanceNorm2d(channel_out)]
+        nn.init.xavier_uniform_(layers[0].weight)
+    else:
+        layers += [nn.Conv2d(channel_in, channel_out, kernel_size, stride, padding, bias=False)]
+        nn.init.xavier_uniform_(layers[0].weight)
+        
+    if activation.lower() == 'relu':
+        layers += [nn.ReLU(inplace=True)]
+    if activation.lower() == 'leakyrelu':
+        layers += [nn.LeakyReLU(0.1, inplace=True)]
+    if activation.lower() == 'tanh':
+        layers += [nn.Tanh()]
+    if activation.lower() == 'sigmoid':
+        layers += [nn.Sigmoid()]
+    
+        
+    return nn.Sequential(*layers)
+        
+def upconv2d(params_list, instance_norm = True):
+    channel_in, channel_out, kernel_size, stride, padding, activation = params_list
+    layers = []
+    if instance_norm:
+        layers += [nn.ConvTranspose2d(channel_in, channel_out, kernel_size, stride, padding, bias=False)]
+        nn.init.xavier_uniform_(layers[0].weight)
+    else:
+        layers += [nn.ConvTranspose2d(channel_in, channel_out, kernel_size, stride, padding, bias=False)]
+        nn.init.xavier_uniform_(layers[0].weight)
+        
+    if activation.lower() == 'relu':
+        layers += [nn.ReLU(inplace=True)]
+    if activation.lower() == 'leakyrelu':
+        layers += [nn.LeakyReLU(0.1, inplace=True)]
+    if activation.lower() == 'tanh':
+        layers += [nn.Tanh()]
+    if activation.lower() == 'sigmoid':
+        layers += [nn.Sigmoid()]
+        
+    return nn.Sequential(*layers)
 
+def transpose(ndarray):
+    return np.transpose(ndarray, [0,2,3,1])
